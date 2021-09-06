7475d5c5f56a348f6e74a5d1b1ad31b8c7f62ef7
jackmoo <51739356+a31663835@users.noreply.github.com>
2021-09-06 18:37:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+#change kernel
+sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/x86/Makefile
